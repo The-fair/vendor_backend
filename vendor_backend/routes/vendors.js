9dce3e29ad3c4016997617e9999dee2516fc70dc
jr <renjie@whu.edu.cn>
2020-05-27 22:01:20 -0400

    inprogess

--- vendor_backend/routes/vendors.js
+++ vendor_backend/routes/vendors.js
@@ -2,3 +2,5 @@ var express = require('express');
 var router = express.Router();
 var mongoose = require(mongoose);
 
+//parse and validate the http request received here
+//call the savevendor function provided by orm.js 
